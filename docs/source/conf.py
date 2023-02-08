4d5e4d3921ba38c83b39503fe3190af315987816
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2023-02-09 00:43:00 +0900

    project start

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+from typing import Dict, Optional, Any
+import os
+import sys
+
+from sphinx_pyproject_poetry import SphinxConfig
+
+sys.path.insert(0, os.path.abspath("../../mypy_safe_unpack"))
+config: SphinxConfig = SphinxConfig("../../pyproject.toml", globalns=globals())
+project: str = config.name
+
+
+def linkcode_resolve(
+    domain: str,
+    info: Dict[str, Any],
+) -> Optional[str]:
+    if domain != "py":
+        return None
+    if not info["module"]:
+        return None
+    filename = info["module"].replace(".", "/")
+    if filename == "mypy_safe_unpack":
+        filename += "/__init__.py"
+    elif not filename.endswith(".py"):
+        filename += ".py"
+    return (
+        f"{config.repository}/blob/main/{filename}"
+    )
