462cf6b17e4aec12a00512f461c41f8d244daae4
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2023-02-09 12:23:11 +0900

    v0.2.1

--- mypy_safe_unpack/__init__.py
+++ mypy_safe_unpack/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-__version__ = "0.2.0"  # Automatically updated by poetry-dynamic-versioning
+__version__ = "0.2.1"  # Automatically updated by poetry-dynamic-versioning
 __all__ = ["__version__"]
